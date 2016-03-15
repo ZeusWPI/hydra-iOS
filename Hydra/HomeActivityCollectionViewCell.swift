d0ceffdb94fee5877e7fe69312d2185ecadeaa8b
Feliciaan De Palmenaer <feliciaan@de-palmenaer.eu>
2016-03-15 22:43:45 +0100

    Added associations shields to the iOS cards

--- Hydra/HomeActivityCollectionViewCell.swift
+++ Hydra/HomeActivityCollectionViewCell.swift
@@ -59,8 +59,10 @@ class HomeActivityCollectionViewCell: UICollectionViewCell {
             
             if let url = activity?.facebookEvent?.smallImageUrl {
                 imageView.sd_setImageWithURL(url, placeholderImage: imageView.image)
+            } else if let association = activity?.association.internalName.lowercaseString {
+                imageView.sd_setImageWithURL(NSURL(string: "https://zeus.ugent.be/hydra/api/2.0/association/logo/\(association).jpg")!)
             } else {
-                imageView.image = nil
+                imageView = nil
             }
         }
     }
