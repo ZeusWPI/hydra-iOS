10dc46234a2f0e55a599132f04c3829eed7651e3
Feliciaan De Palmenaer <feliciaan@de-palmenaer.eu>
2016-03-30 19:30:40 +0200

    Fix double space:

--- Hydra/InfoViewController.m
+++ Hydra/InfoViewController.m
@@ -139,7 +139,7 @@
     else if(item[@"url"]) {
         NSURL *url = [NSURL URLWithString:item[@"url"]];
         if (IOS_VERSION_GREATER_THAN_OR_EQUAL_TO(@"9")) {
-            SFSafariViewController *svc = [[SFSafariViewController alloc] initWithURL:url];
+            SFSafariViewController *svc = [[SFSafariViewController alloc]  initWithURL:url];
             [self.navigationController presentViewController:svc animated:YES completion:nil];
         } else {
             [[UIApplication sharedApplication] openURL:url];
